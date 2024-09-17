900afe67960af614ea4b47ca054a3f16d9864fd6
sunnd4y <120350465+sunnd4y@users.noreply.github.com>
2024-09-17 21:37:00 +0700

    Update GetDataInfo.lua

--- GetDataInfo.lua
+++ GetDataInfo.lua
@@ -1,7 +1,7 @@
-local _Load = loadstring
-local _Print = print
-local Data = _Load(game:HttpGet("https://raw.githubusercontent.com/sunnd4y/sunnd4y/main/InfoData.lua"))()
+local _Load = loadstring;
+local _Print = print;
+local Data = _Load(game:HttpGet("https://raw.githubusercontent.com/sunnd4y/sunnd4y/main/InfoData.lua"))();
 
 for Index, Value in pairs(Data) do 
    _Print(Index, Value)
-end
+end;
